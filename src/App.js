4696fc0c6e355cf5a20559ce8a407dfb0acbe83c
MatRising <osmiorniczkakropelka@gmail.com>
2022-01-23 15:05:19 +0100

    Added toggl and slider car status controlls

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ import {
 } from "./consts/additionalMarkerks";
 import { determineIconType } from "./helpers/iconType";
 import { determineImageType } from "./helpers/imageType";
+import MapControlls from "./components/MapControlls";
 
 const ClickEvent = () => {
   const map = useMapEvent("click", (e) => {
@@ -28,7 +29,13 @@ const ClickEvent = () => {
 const App = () => {
   const [cars, setCars] = useState([]);
   const [batteryThreshold, setBatteryTreshold] = useState(0);
-  const [carStatus, setCarStatus] = useState();
+  const [carStatus, setCarStatus] = useState(false);
+  const changeBatteryThreshold = (batteryThreshold) => {
+    setBatteryTreshold(batteryThreshold);
+  };
+  const changeCarStatus = (carStatus) => {
+    setCarStatus(carStatus);
+  };
   useEffect(() => {
     fetch("https://dev.vozilla.pl/api-client-portal/map?objectType=VEHICLE")
       .then((response) => response.json())
@@ -36,115 +43,104 @@ const App = () => {
   }, []);
   return (
     <div className="main-container">
-      <div className="controlls">
-        <div>
-          <label>
-            <input
-              type="checkbox"
-              id="availability"
-              name="isAvailable"
-              value={carStatus}
-              onChange={(e) => setCarStatus(!carStatus)}
-            ></input>
-            {carStatus ? "Only Available" : "ALL"}
-          </label>
-        </div>
-        <div>
-          <input
-            type="range"
-            min="0"
-            max="100"
-            value={batteryThreshold}
-            onChange={(e) => setBatteryTreshold(e.target.value)}
-          ></input>
-        </div>
-        <div style={{ textAlign: "center" }}>
-          {"Battery: " + batteryThreshold + "%+"}
-        </div>
-      </div>
-      <MapContainer
-        className="map"
-        center={[52.1935161702226, 20.9304286193486]}
-        zoom={16}
-        scrollWheelZoom={true}
-      >
-        <ClickEvent></ClickEvent>
-        <LayersControl>
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <LayersControl.Overlay checked name="Cars">
-            <MarkerClusterGroup
-              showCoverageOnHover={false}
-              disableClusteringAtZoom={18}
-            >
-              {cars
-                .filter((car) => car.batteryLevelPct >= batteryThreshold)
-                .filter((car) =>
-                  carStatus === true ? car.status === "AVAILABLE" : true
-                )
-                .map((car) => {
+      <div className="map-wrapper">
+        <MapControlls
+          batteryThreshold={batteryThreshold}
+          carStatus={carStatus}
+          changeBatteryThreshold={changeBatteryThreshold}
+          changeCarStatus={changeCarStatus}
+        ></MapControlls>
+        <MapContainer
+          className="map"
+          center={[52.1935161702226, 20.9304286193486]}
+          zoom={16}
+          scrollWheelZoom={true}
+        >
+          <ClickEvent></ClickEvent>
+          <LayersControl>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <LayersControl.Overlay checked name="Cars">
+              <MarkerClusterGroup
+                showCoverageOnHover={false}
+                disableClusteringAtZoom={18}
+              >
+                {cars
+                  .filter((car) => car.batteryLevelPct >= batteryThreshold)
+                  .filter((car) =>
+                    carStatus === true ? car.status === "AVAILABLE" : true
+                  )
+                  .map((car) => {
+                    return (
+                      <Marker
+                        key={car.id}
+                        position={[
+                          car.location.latitude,
+                          car.location.longitude,
+                        ]}
+                        icon={icon(
+                          determineIconType(car.type, car.batteryLevelPct)
+                        )}
+                      >
+                        <Popup>
+                          {car.name}
+                          <br />
+                          {"Plate: " + car.platesNumber}
+                          <br />
+                          {"Battery " + car.batteryLevelPct + "%"}
+                          <br />
+                          {"Range " + car.rangeKm + "km"}
+                          <br />
+                          {car.status === "AVAILABLE"
+                            ? "Car available"
+                            : "Car not available"}
+                          <br />
+                          {determineImageType(car.type)}
+                        </Popup>
+                      </Marker>
+                    );
+                  })}
+              </MarkerClusterGroup>
+            </LayersControl.Overlay>
+            <LayersControl.Overlay name="Parking">
+              <LayerGroup>
+                {dummyDataParkings.map((parking) => {
                   return (
                     <Marker
-                      key={car.id}
-                      position={[car.location.latitude, car.location.longitude]}
-                      icon={icon(
-                        determineIconType(car.type, car.batteryLevelPct)
-                      )}
+                      key={parking.id}
+                      position={[
+                        parking.location.latitude,
+                        parking.location.longitude,
+                      ]}
                     >
-                      <Popup>
-                        {car.name}
-                        <br />
-                        {"Plate: " + car.platesNumber}
-                        <br />
-                        {"Battery " + car.batteryLevelPct + "%"}
-                        <br />
-                        {"Range " + car.rangeKm + "km"}
-                        <br />
-                        {determineImageType(car.type)}
-                      </Popup>
+                      <Tooltip>{parking.name}</Tooltip>
                     </Marker>
                   );
                 })}
-            </MarkerClusterGroup>
-          </LayersControl.Overlay>
-          <LayersControl.Overlay name="Parking">
-            <LayerGroup>
-              {dummyDataParkings.map((parking) => {
-                return (
-                  <Marker
-                    key={parking.id}
-                    position={[
-                      parking.location.latitude,
-                      parking.location.longitude,
-                    ]}
-                  >
-                    <Tooltip>{parking.name}</Tooltip>
-                  </Marker>
-                );
-              })}
-            </LayerGroup>
-          </LayersControl.Overlay>
-          <LayersControl.Overlay name="POI">
-            <LayerGroup>
-              {dummyDataPOI.map((parking) => {
-                return (
-                  <Marker
-                    key={parking.id}
-                    position={[
-                      parking.location.latitude,
-                      parking.location.longitude,
-                    ]}
-                  >
-                    <Tooltip>{parking.name}</Tooltip>
-                  </Marker>
-                );
-              })}
-            </LayerGroup>
-          </LayersControl.Overlay>
-        </LayersControl>
-      </MapContainer>
+              </LayerGroup>
+            </LayersControl.Overlay>
+            <LayersControl.Overlay name="POI">
+              <LayerGroup>
+                {dummyDataPOI.map((parking) => {
+                  return (
+                    <Marker
+                      key={parking.id}
+                      position={[
+                        parking.location.latitude,
+                        parking.location.longitude,
+                      ]}
+                    >
+                      <Tooltip>{parking.name}</Tooltip>
+                    </Marker>
+                  );
+                })}
+              </LayerGroup>
+            </LayersControl.Overlay>
+          </LayersControl>
+        </MapContainer>
+      </div>
     </div>
   );
 };
---
+++ src/components/MapControlls.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./mapcontroll.css";
+
+const MapControlls = ({
+  batteryThreshold,
+  carStatus,
+  changeBatteryThreshold,
+  changeCarStatus,
+}) => {
+  return (
+    <div className="controlls">
+      <div>
+        <label className="toggle">
+          <input
+            type="checkbox"
+            id="availability"
+            name="isAvailable"
+            value={carStatus}
+            onChange={() => changeCarStatus(!carStatus)}
+          ></input>
+          <span class="toggle-slider"></span>
+        </label>
+        {carStatus ? "Available" : "All cars"}
+      </div>
+      <div className="divider"></div>
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="100"
+          value={batteryThreshold}
+          onChange={(e) => changeBatteryThreshold(e.target.value)}
+        ></input>
+      </div>
+      <div style={{ textAlign: "center" }}>
+        {"Battery > " + batteryThreshold + "%"}
+      </div>
+    </div>
+  );
+};
+
+export default MapControlls;
